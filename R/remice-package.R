2aaf56092f985939d5dff0c4f1a6f5a6afc7bcdd
Eric Tong <erictong@college.harvard.edu>
2024-12-10 15:57:19 -0500

    Added file for ?remice

---
+++ R/remice-package.R
@@ -0,0 +1,15 @@
+#' remice: Tools for Cleaning Mice Data
+#'
+#' The remice package provides tools for cleaning and analyzing datasets,
+#' including those derived from experiments involving mice.
+#'
+#' @section Online Documentation:
+#' For a complete overview of the package, visit the [remice documentation website](https://erictong2.github.io/remice).
+#'
+#' @section Functions:
+#' - \code{\link{analyze_type}}: Analyze the entries of a column not of a specific type
+#' - \code{\link{plot_outliers}}: Visualize any data points with significant change
+#' - \code{\link{separate_list}}: Separate multiple entries in a single cell
+#' - \code{\link{table_different}}: Create a table of any unique character values
+#'
+"_PACKAGE"
